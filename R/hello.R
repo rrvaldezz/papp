0bf3af4b2631a41ec337a3d539091be114089e8b
rrvaldezz <romario.12.64@gmail.com>
2025-02-12 18:10:36 -0500

    ver1

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,4 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
+
 mul_sum <- function(a,b){
   if(b==0){
     return(0)
@@ -11,9 +8,7 @@ mul_sum <- function(a,b){
 }
 cat("PROGRAMA-MULTIPLICACION DE 2 NUMEROS A TRAVÉS DE SUMAS SUCESIVAS")
 cat("\n Ingrese el primer numero:")
-a <- readLines(n=1)
-a <- as.numeric(a)
+a <- as.numeric(readline())
 cat("Ingrese el segundo numero:")
-b <- readLines(n=1)
-b <- as.numeric(b)
+b <- as.numeric(readline())
 cat("El resultado de multiplicar",a,"por",b,"es:", mul_sum(a,b))
